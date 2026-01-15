8c29dd3cd27c6000b16ae79359f592fdd9da4e6f
comagale-art <comagale@gmail.com>
2026-01-15 12:31:52 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,30 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
