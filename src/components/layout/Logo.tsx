14ec54b3c7d5a8e976c013ca599a73b3fd1f8ca4
comagale-art <comagale@gmail.com>
2026-03-07 13:44:40 +0000

    Updated Logo.tsx

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -7,7 +7,7 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ className = '' }) => {
   return (
     <img
-      src="https://comagale.com/assets/logo-comagal-BGb6Fdik.png"
+      src="https://gestionclientcomagal.lovable.app/assets/logo-comagal-BGb6Fdik.png"
       alt="Comagal Energy Logo"
       className={`h-10 w-auto object-contain ${className}`}
     />
