d890c55f005358440f48366af07252cfc3754b4b
comagale-art <comagale@gmail.com>
2026-06-16 12:41:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
@@ -10,6 +9,15 @@ export default defineConfig({
   },
 
   build: {
-    chunkSizeWarningLimit: 1500,
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          supabase: ['@supabase/supabase-js'],
+        },
+      },
+    },
   },
 });
